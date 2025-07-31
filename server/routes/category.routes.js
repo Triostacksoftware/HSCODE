b561f9e41a52ad10b08c40ccf9a0123500bff80a
PrashantSHIV <prashantkumar156578@gmail.com>
2025-07-31 13:14:42 +0530

    FIX: Bulk File Routs

--- server/routes/category.routes.js
+++ server/routes/category.routes.js
@@ -12,21 +12,21 @@ import {
   createManyCategory,
   createManyGroup,
 } from "../controllers/category.ctrls.js";
-import upload, { uploadXLSX } from "../configurations/multer.js";
+import upload from "../configurations/multer.js";
 
 const router = express.Router();
 
 // Category routes
 router.get("/", authMiddleware, getCategories);
 router.post("/", authMiddleware, createCategory);
-router.post("/many", authMiddleware, uploadXLSX.single('file'), createManyCategory);
+router.post("/many", authMiddleware, upload.single('file'), createManyCategory);
 router.delete("/:id", authMiddleware, deleteCategory);
 router.patch("/:id", authMiddleware, updateCategory);
 
 // Group routes inside category
 router.get("/:id/group", authMiddleware, getGroups);
 router.post("/:id/group", authMiddleware, upload.single("file"), createGroup);
-router.post("/:id/group/many", authMiddleware, uploadXLSX.single("file"), createManyGroup);
+router.post("/:id/group/many", authMiddleware, upload.single("file"), createManyGroup);
 router.patch("/:id/group/:groupId", authMiddleware, updateGroup);
 router.delete("/:id/group/:groupId", authMiddleware, deleteGroup);
 
